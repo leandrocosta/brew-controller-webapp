ec1257843d3f19b44dd2ca3f1b698a0510704565
Leandro Costa <leandro.costa@gmail.com>
2015-08-06 19:30:35 -0300

    refactoring

--- app/track/button-play-pause.drv.js
+++ app/track/button-play-pause.drv.js
@@ -12,8 +12,8 @@
                 /*template: '<button-base on-click="onClick()" arial-label="Play / pause" icon="play_arrow" class="md-fab" fill="white"></button-base>'*/
                 template: '<md-button ng-click="onClick()" ng-disabled="myDisabled === ' + "'true'" + '" ng-enter="onClick()" ' +
                     'class="md-fab" aria-label="Play / pause" ng-mouseenter="mouseover=true" ng-mouseleave="mouseover=false" ng-class="{' + "'md-focused'" + ':mouseover}">' +
-                    '<button-icon icon="play_arrow" ng-show="showPlay === ' + "'true'" + '" fill="white"></button-icon>' +
-                    '<button-icon icon="pause" ng-show="showPause === ' + "'true'" + '" fill="white"></button-icon>' +
+                    '<button-icon icon="play_arrow" ng-if="showPlay === ' + "'true'" + '" fill="white"></button-icon>' +
+                    '<button-icon icon="pause" ng-if="showPause === ' + "'true'" + '" fill="white"></button-icon>' +
                     '</md-button>'
             };
         });
--- app/track/track-head.drv.js
+++ app/track/track-head.drv.js
@@ -4,7 +4,7 @@
     angular.module('App')
         .directive('trackHead', function() {
             return {
-                template: '<span class="md-subhead" style="white-space: nowrap;">Controlador {{$index+1}}</span>' +
+                template: '<span class="md-subhead" style="white-space: nowrap;">Controller {{$index+1}}</span>' +
                     '<ng-transclude layout="column" layout-align="center center"></ng-transclude>',
                 transclude: true
             };
